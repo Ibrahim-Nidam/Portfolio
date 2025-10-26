b2cd0c8c2b8e9906f1599801e9f523d2472cdb43
GitHub Action <action@github.com>
2025-10-26 01:06:10 +0000

    chore: update repos data

--- dist/repos.js
+++ dist/repos.js
@@ -1,21 +1,5 @@
 export const repos = [
   {
-    "id": 1079875688,
-    "name": "Delivery_Optimizer",
-    "description": "Conception et implémentation d'une API REST Spring Boot de gestion optimisée de tournées de livraison permettant la planification intelligente des itinéraires selon les contraintes de capacité et de distance des véhicules",
-    "html_url": "https://github.com/Ibrahim-Nidam/Delivery_Optimizer",
-    "language": "Java",
-    "languages": [
-      "Java"
-    ],
-    "stargazers_count": 0,
-    "forks_count": 0,
-    "updated_at": "2025-10-24T15:59:05Z",
-    "topics": [],
-    "fork": false,
-    "private": false
-  },
-  {
     "id": 1077591749,
     "name": "Portfolio",
     "description": "📱 Modern developer portfolio with automated GitHub repo syncing, multi-language filtering, dark mode, and zero backend. Built with TypeScript, Tailwind CSS, and GitHub Actions for daily auto-updates.",
@@ -29,7 +13,23 @@ export const repos = [
     ],
     "stargazers_count": 0,
     "forks_count": 0,
-    "updated_at": "2025-10-24T00:58:45Z",
+    "updated_at": "2025-10-25T01:00:08Z",
+    "topics": [],
+    "fork": false,
+    "private": false
+  },
+  {
+    "id": 1079875688,
+    "name": "Delivery_Optimizer",
+    "description": "Conception et implémentation d'une API REST Spring Boot de gestion optimisée de tournées de livraison permettant la planification intelligente des itinéraires selon les contraintes de capacité et de distance des véhicules",
+    "html_url": "https://github.com/Ibrahim-Nidam/Delivery_Optimizer",
+    "language": "Java",
+    "languages": [
+      "Java"
+    ],
+    "stargazers_count": 0,
+    "forks_count": 0,
+    "updated_at": "2025-10-24T15:59:05Z",
     "topics": [],
     "fork": false,
     "private": false
