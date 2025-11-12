5fa34cbff0dd5797892e2fb9d4387a77ce434f80
GitHub Action <action@github.com>
2025-11-12 01:04:02 +0000

    chore: update repos data

--- dist/repos.js
+++ dist/repos.js
@@ -16,6 +16,23 @@ export const repos = [
     "private": false
   },
   {
+    "id": 1086373801,
+    "name": "Modoria_Fil_Rouge",
+    "description": "Modoria aims to revolutionize this experience through an intelligent e-commerce website that dynamically changes its design, atmosphere, and clothing collections according to the seasons (Spring, Summer, Autumn, Winter).",
+    "html_url": "https://github.com/Ibrahim-Nidam/Modoria_Fil_Rouge",
+    "language": "Java",
+    "languages": [
+      "Java",
+      "Dockerfile"
+    ],
+    "stargazers_count": 0,
+    "forks_count": 0,
+    "updated_at": "2025-11-11T16:06:54Z",
+    "topics": [],
+    "fork": false,
+    "private": false
+  },
+  {
     "id": 1077591749,
     "name": "Portfolio",
     "description": "📱 Modern developer portfolio with automated GitHub repo syncing, multi-language filtering, dark mode, and zero backend. Built with TypeScript, Tailwind CSS, and GitHub Actions for daily auto-updates.",
@@ -29,7 +46,7 @@ export const repos = [
     ],
     "stargazers_count": 0,
     "forks_count": 0,
-    "updated_at": "2025-11-10T01:06:48Z",
+    "updated_at": "2025-11-11T01:04:49Z",
     "topics": [],
     "fork": false,
     "private": false
@@ -46,20 +63,6 @@ export const repos = [
     "stargazers_count": 0,
     "forks_count": 0,
     "updated_at": "2025-11-09T17:22:49Z",
-    "topics": [],
-    "fork": false,
-    "private": false
-  },
-  {
-    "id": 1086373801,
-    "name": "Modoria_Fil_Rouge",
-    "description": "Modoria aims to revolutionize this experience through an intelligent e-commerce website that dynamically changes its design, atmosphere, and clothing collections according to the seasons (Spring, Summer, Autumn, Winter).",
-    "html_url": "https://github.com/Ibrahim-Nidam/Modoria_Fil_Rouge",
-    "language": null,
-    "languages": [],
-    "stargazers_count": 0,
-    "forks_count": 0,
-    "updated_at": "2025-10-30T10:30:54Z",
     "topics": [],
     "fork": false,
     "private": false
