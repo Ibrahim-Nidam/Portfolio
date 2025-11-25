c425a99ac07b6df8c44407a5cca42afaec6d68ee
GitHub Action <action@github.com>
2025-11-25 01:03:50 +0000

    chore: update repos data

--- dist/repos.js
+++ dist/repos.js
@@ -16,6 +16,22 @@ export const repos = [
     "private": false
   },
   {
+    "id": 1102939660,
+    "name": "Croise_SmartShop",
+    "description": null,
+    "html_url": "https://github.com/Ibrahim-Nidam/Croise_SmartShop",
+    "language": "Java",
+    "languages": [
+      "Java"
+    ],
+    "stargazers_count": 0,
+    "forks_count": 0,
+    "updated_at": "2025-11-24T21:22:36Z",
+    "topics": [],
+    "fork": false,
+    "private": false
+  },
+  {
     "id": 1077591749,
     "name": "Portfolio",
     "description": "📱 Modern developer portfolio with automated GitHub repo syncing, multi-language filtering, dark mode, and zero backend. Built with TypeScript, Tailwind CSS, and GitHub Actions for daily auto-updates.",
@@ -29,7 +45,7 @@ export const repos = [
     ],
     "stargazers_count": 0,
     "forks_count": 0,
-    "updated_at": "2025-11-23T01:12:41Z",
+    "updated_at": "2025-11-24T01:08:30Z",
     "topics": [],
     "fork": false,
     "private": false
