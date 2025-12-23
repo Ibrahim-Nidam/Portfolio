91f0982cdbadc91ead0d1a94a6e1ea3d488d6b65
GitHub Action <action@github.com>
2025-12-23 01:08:05 +0000

    chore: update repos data

--- dist/repos.js
+++ dist/repos.js
@@ -16,6 +16,23 @@ export const repos = [
     "private": false
   },
   {
+    "id": 1086373801,
+    "name": "Modoria_Fil_Rouge",
+    "description": "Modoria aims to revolutionize this experience through an intelligent e-commerce website that dynamically changes its design, atmosphere, and clothing collections according to the seasons (Spring, Summer, Autumn, Winter).",
+    "html_url": "https://github.com/Ibrahim-Nidam/Modoria_Fil_Rouge",
+    "language": "Java",
+    "languages": [
+      "Java",
+      "Dockerfile"
+    ],
+    "stargazers_count": 0,
+    "forks_count": 0,
+    "updated_at": "2025-12-22T11:15:32Z",
+    "topics": [],
+    "fork": false,
+    "private": false
+  },
+  {
     "id": 1077591749,
     "name": "Portfolio",
     "description": "📱 Modern developer portfolio with automated GitHub repo syncing, multi-language filtering, dark mode, and zero backend. Built with TypeScript, Tailwind CSS, and GitHub Actions for daily auto-updates.",
@@ -29,7 +46,7 @@ export const repos = [
     ],
     "stargazers_count": 0,
     "forks_count": 0,
-    "updated_at": "2025-12-21T01:12:24Z",
+    "updated_at": "2025-12-22T01:11:08Z",
     "topics": [],
     "fork": false,
     "private": false
@@ -63,23 +80,6 @@ export const repos = [
     "stargazers_count": 0,
     "forks_count": 0,
     "updated_at": "2025-11-28T16:26:31Z",
-    "topics": [],
-    "fork": false,
-    "private": false
-  },
-  {
-    "id": 1086373801,
-    "name": "Modoria_Fil_Rouge",
-    "description": "Modoria aims to revolutionize this experience through an intelligent e-commerce website that dynamically changes its design, atmosphere, and clothing collections according to the seasons (Spring, Summer, Autumn, Winter).",
-    "html_url": "https://github.com/Ibrahim-Nidam/Modoria_Fil_Rouge",
-    "language": "Java",
-    "languages": [
-      "Java",
-      "Dockerfile"
-    ],
-    "stargazers_count": 0,
-    "forks_count": 0,
-    "updated_at": "2025-11-17T13:17:04Z",
     "topics": [],
     "fork": false,
     "private": false
