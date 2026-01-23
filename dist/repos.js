a167930341a2c1fb2527a9b56cae181e9c07c105
GitHub Action <action@github.com>
2026-01-23 01:11:26 +0000

    chore: update repos data

--- dist/repos.js
+++ dist/repos.js
@@ -16,6 +16,25 @@ export const repos = [
     "private": false
   },
   {
+    "id": 1077591749,
+    "name": "Portfolio",
+    "description": "📱 Modern developer portfolio with automated GitHub repo syncing, multi-language filtering, dark mode, and zero backend. Built with TypeScript, Tailwind CSS, and GitHub Actions for daily auto-updates.",
+    "html_url": "https://github.com/Ibrahim-Nidam/Portfolio",
+    "language": "TypeScript",
+    "languages": [
+      "TypeScript",
+      "HTML",
+      "JavaScript",
+      "CSS"
+    ],
+    "stargazers_count": 0,
+    "forks_count": 0,
+    "updated_at": "2026-01-22T01:12:28Z",
+    "topics": [],
+    "fork": false,
+    "private": false
+  },
+  {
     "id": 1138975167,
     "name": "oaqjp-final-project-emb-ai",
     "description": "final-project-emb-ai",
@@ -30,25 +49,6 @@ export const repos = [
     "updated_at": "2026-01-21T11:03:24Z",
     "topics": [],
     "fork": true,
-    "private": false
-  },
-  {
-    "id": 1077591749,
-    "name": "Portfolio",
-    "description": "📱 Modern developer portfolio with automated GitHub repo syncing, multi-language filtering, dark mode, and zero backend. Built with TypeScript, Tailwind CSS, and GitHub Actions for daily auto-updates.",
-    "html_url": "https://github.com/Ibrahim-Nidam/Portfolio",
-    "language": "TypeScript",
-    "languages": [
-      "TypeScript",
-      "HTML",
-      "JavaScript",
-      "CSS"
-    ],
-    "stargazers_count": 0,
-    "forks_count": 0,
-    "updated_at": "2026-01-21T01:12:31Z",
-    "topics": [],
-    "fork": false,
     "private": false
   },
   {
